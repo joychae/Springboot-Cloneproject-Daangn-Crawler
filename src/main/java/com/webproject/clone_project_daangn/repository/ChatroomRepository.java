dbd465e7290633c09db9d5abdda191809e706158
joychae <sooie35@gmail.com>
2021-04-06 20:06:18 +0900

    code refactoring

--- src/main/java/com/webproject/clone_project_daangn/repository/ChatroomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.webproject.clone_project_daangn.repository;
-
-import com.webproject.clone_project_daangn.model.entity.Chatroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
-}
